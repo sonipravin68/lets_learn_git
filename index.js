fcb37009091d0827e46bb1e005654f0c744e591f
Pravin soni <soniipravin68@gmail.com>
2023-04-22 08:32:58 +0530

    create plus fuction

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const arr1 = [1, 2, 3, 4, 5];
 const [ayush, soni, how, are, you] = arr1;
 
 console.log(`i am number ${ayush}`);
+
+const plus = (a, b) => {
+  return a + b;
+};
+
+const addition = plus(2, 3);
+console.log(addition);
